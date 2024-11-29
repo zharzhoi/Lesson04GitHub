90fece9d6772203132b8a0c5a2e81f1f4ac9dd0d
Aleksei Aleksei <zharzhoi@yandex.com>
2024-11-29 21:43:34 +0300

    Новая ветка

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson04GitHub
+{
+    internal class Class1
+    {
+    }
+}
